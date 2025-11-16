cdd4be3ce038bc93db8896f6be2fffdeeba13394
pinczesbalint977 <pinczesbalint1997>
2025-11-16 15:48:44 +0100

    javított

--- script.js
+++ script.js
@@ -6,4 +6,35 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 
+document.body.classList.add('loading');
 
+
+// Wait until EVERYTHING is fully loaded (images, fonts, scripts)
+const waitFullLoad = setInterval(() => {
+if (document.readyState === "complete") {
+const overlay = document.getElementById('loading-overlay');
+overlay.style.opacity = "0";
+overlay.style.transition = "0.5s ease";
+setTimeout(() => overlay.remove(), 500);
+document.body.classList.remove('loading');
+clearInterval(waitFullLoad);
+}
+}, 500);('loading');
+
+
+// Cookie bar
+document.addEventListener("DOMContentLoaded", () => {
+const cookieBar = document.getElementById('cookie-bar');
+const acceptBtn = document.getElementById('cookie-accept');
+
+
+if (!localStorage.getItem('cookieAccepted')) {
+cookieBar.classList.remove('hidden');
+}
+
+
+acceptBtn.addEventListener('click', () => {
+localStorage.setItem('cookieAccepted', 'true');
+cookieBar.classList.add('hidden');
+});
+});
